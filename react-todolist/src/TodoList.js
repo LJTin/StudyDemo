2f074805d0a2681eec37a0166accb5e434fa6a88
LJTin <jiantinglian619@163.com>
2019-06-14 10:41:41 +0800

    redux使用

--- react-todolist/src/TodoList.js
+++ react-todolist/src/TodoList.js
@@ -7,21 +7,53 @@ class TodoList extends Component{
   constructor(props){
     super(props);
     this.state = store.getState()
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleStoreChange = this.handleStoreChange.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+    store.subscribe(this.handleStoreChange)
   }
   render(){
     return (
       <div>
         <div>
-          <Input placeholder='todoinfo' style={{width:300}} value={this.state.inputValue}/>
-          <Button type='primary'>tijiao</Button>
+          <Input placeholder='todoinfo' style={{width:300}} value={this.state.inputValue}
+          onChange={this.handleInputChange}
+          />
+          <Button type='primary'
+            onClick={this.handleClick}
+          >tijiao</Button>
         </div>
         <List
           bordered
           dataSource={this.state.list}
-          renderItem={item=>(<List.Item>{item}</List.Item>)}
+          renderItem={(item,index)=>(<List.Item onClick={this.handleItemDelete.bind(this,index)}>{item}</List.Item>)}
         ></List>
       </div>
     )
+  }
+  handleInputChange(e) {
+    const action = {
+      type:'change_input_value',
+      value: e.target.value
+    }
+    store.dispatch(action)
+  }
+  handleStoreChange () {
+    this.setState(store.getState())
+  }
+  handleClick () {
+    const action = {
+      type:'add_todo_item',
+    }
+    store.dispatch(action)
+  }
+  handleItemDelete (index) {
+    console.log(index)
+    const action ={
+      type:'delete_item',
+      index
+    }
+    store.dispatch(action)
   }
 }
 
--- react-todolist/src/store/reducer.js
+++ react-todolist/src/store/reducer.js
@@ -2,7 +2,24 @@ const defaultState = {
   inputValue:'23',
   list:[1,2,3,4]
 }
-
+// reducer可以接收state，但不能修改，符合单向数据流
 export default (state = defaultState, action)=>{
+  if(action.type=== 'change_input_value'){
+    const newState = JSON.parse(JSON.stringify(state));
+    newState.inputValue =  action.value;
+    return newState
+  }
+  if(action.type === 'add_todo_item'){
+    const newState = JSON.parse(JSON.stringify(state));
+    newState.list.push(newState.inputValue)
+    newState.inputValue = ''
+    console.log(newState)
+    return newState
+  }
+  if(action.type === 'delete_item'){
+    const newState = JSON.parse(JSON.stringify(state));
+    newState.list.splice(action.index,1)
+    return newState
+  }
   return state;
 }
